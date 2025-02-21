b1a39dd331b3153f597a72bcda9063a7234f3b25
liyanatasha <liyananatasha@hotmail.com>
2025-02-21 11:09:30 +0800

    bug fix

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
 
 // Add session middleware
 app.use(session({
